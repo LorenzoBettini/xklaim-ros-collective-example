037cae0ceae03ec87dcc5f2c43d327ba101fa56b
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-01-27 11:10:53 +0100

    default Duration in JointTrajectoryPoint

--- Robot_arm/src-gen/xklaim/coordination/After_pick.java
+++ Robot_arm/src-gen/xklaim/coordination/After_pick.java
@@ -1,7 +1,6 @@
 package xklaim.coordination;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import coordination.Duration;
 import coordination.JointTrajectory;
 import coordination.JointTrajectoryPoint;
 import java.util.Arrays;
@@ -42,10 +41,9 @@ public class After_pick extends KlavaProcess {
       double tol = 0.007;
       if ((norm <= tol)) {
         InputOutput.<String>println(String.format("I picked the object"));
-        JointTrajectoryPoint _positions = new JointTrajectoryPoint().positions(
-          ((double[])Conversions.unwrapArray(Arrays.<Double>asList(Double.valueOf((-3.1415)), Double.valueOf((-0.2862)), Double.valueOf((-0.5000)), Double.valueOf(3.1400), Double.valueOf(1.6613), Double.valueOf((-0.0142))), double.class)));
-        Duration _duration = new Duration(120, 0);
-        final JointTrajectoryPoint jointTrajectoryPoints = _positions.timeFromStart(_duration);
+        final JointTrajectoryPoint jointTrajectoryPoints = new JointTrajectoryPoint().positions(
+          ((double[])Conversions.unwrapArray(Arrays.<Double>asList(
+            Double.valueOf((-3.1415)), Double.valueOf((-0.2862)), Double.valueOf((-0.5000)), Double.valueOf(3.1400), Double.valueOf(1.6613), Double.valueOf((-0.0142))), double.class)));
         final JointTrajectory afterPick = new JointTrajectory().points(((JointTrajectoryPoint[])Conversions.unwrapArray(Arrays.<JointTrajectoryPoint>asList(jointTrajectoryPoints), JointTrajectoryPoint.class))).jointNames(
           ((String[])Conversions.unwrapArray(Arrays.<String>asList("joint1", "joint2", "joint3", "joint4", "joint5", "joint6"), String.class)));
         pub.publish(afterPick);
