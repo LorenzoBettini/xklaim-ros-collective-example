122331762f76914faa0d799b51a7036cba429924
tiezzi <francesco.tiezzi@unifi.it>
2022-02-08 17:34:29 +0100

    refatoring getup process

--- Robot_arm/src-gen/xklaim/coordination/GetUp.java
+++ Robot_arm/src-gen/xklaim/coordination/GetUp.java
@@ -1,10 +1,18 @@
 package xklaim.coordination;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import coordination.JointTrajectory;
+import java.util.Collections;
+import java.util.List;
+import klava.Locality;
 import klava.Tuple;
 import klava.topology.KlavaProcess;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Conversions;
 import ros.Publisher;
 import ros.RosBridge;
+import ros.RosListenDelegate;
+import ros.SubscriptionRequestMsg;
 
 @SuppressWarnings("all")
 public class GetUp extends KlavaProcess {
@@ -17,13 +25,34 @@ public class GetUp extends KlavaProcess {
   
   @Override
   public void executeProcess() {
+    final Locality local = this.self;
     final RosBridge bridge = new RosBridge();
     bridge.connect(this.rosbridgeWebsocketURI, true);
     final Publisher pub = new Publisher("/arm_controller/command", "trajectory_msgs/JointTrajectory", bridge);
-    in(new Tuple(new Object[] {"gripCompleted"}), this.self);
-    final JointTrajectory afterPick = new JointTrajectory().positions(
-      new double[] { (-3.1415), (-0.2862), (-0.5000), 3.1400, 1.6613, (-0.0142) }).jointNames(
+    final List<Double> jointPositions = Collections.<Double>unmodifiableList(CollectionLiterals.<Double>newArrayList(Double.valueOf((-3.1415)), Double.valueOf((-0.2862)), Double.valueOf((-0.5000)), Double.valueOf(3.1400), Double.valueOf(1.6613), Double.valueOf((-0.0142))));
+    final JointTrajectory getUpJointTrajectory = new JointTrajectory().positions(((double[])Conversions.unwrapArray(jointPositions, double.class))).jointNames(
       new String[] { "joint1", "joint2", "joint3", "joint4", "joint5", "joint6" });
-    pub.publish(afterPick);
+    in(new Tuple(new Object[] {"gripCompleted"}), this.self);
+    pub.publish(getUpJointTrajectory);
+    final RosListenDelegate _function = (JsonNode data, String stringRep) -> {
+      final JsonNode actual = data.get("msg").get("actual").get("positions");
+      double delta = 0.0;
+      final double tolerance = 0.008;
+      for (int i = 0; (i < jointPositions.size()); i++) {
+        double _delta = delta;
+        double _asDouble = actual.get(i).asDouble();
+        Double _get = jointPositions.get(i);
+        double _minus = (_asDouble - (_get).doubleValue());
+        double _pow = Math.pow(_minus, 2.0);
+        delta = (_delta + _pow);
+      }
+      final double norm = Math.sqrt(delta);
+      if ((norm <= tolerance)) {
+        out(new Tuple(new Object[] {"getUpCompleted"}), local);
+        bridge.unsubscribe("/arm_controller/state");
+      }
+    };
+    bridge.subscribe(
+      SubscriptionRequestMsg.generate("/arm_controller/state").setType("control_msgs/JointTrajectoryControllerState").setThrottleRate(Integer.valueOf(1)).setQueueLength(Integer.valueOf(1)), _function);
   }
 }
--- Robot_arm/src-gen/xklaim/coordination/Rotate.java
+++ Robot_arm/src-gen/xklaim/coordination/Rotate.java
@@ -1,15 +1,14 @@
 package xklaim.coordination;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import coordination.JointTrajectory;
 import java.util.Collections;
 import java.util.List;
+import klava.Tuple;
 import klava.topology.KlavaProcess;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Conversions;
 import ros.Publisher;
 import ros.RosBridge;
-import ros.RosListenDelegate;
-import ros.SubscriptionRequestMsg;
 
 @SuppressWarnings("all")
 public class Rotate extends KlavaProcess {
@@ -25,29 +24,10 @@ public class Rotate extends KlavaProcess {
     final RosBridge bridge = new RosBridge();
     bridge.connect(this.rosbridgeWebsocketURI, true);
     final Publisher pub = new Publisher("/arm_controller/command", "trajectory_msgs/JointTrajectory", bridge);
-    final RosListenDelegate _function = (JsonNode data, String stringRep) -> {
-      final JsonNode actual = data.get("msg").get("actual").get("positions");
-      final List<Double> desire = Collections.<Double>unmodifiableList(CollectionLiterals.<Double>newArrayList(Double.valueOf((-3.1417061706596003)), Double.valueOf((-0.28618833559546175)), Double.valueOf((-0.49998813405672404)), Double.valueOf(3.1396898889426783), Double.valueOf(1.6612913247682046), Double.valueOf((-0.0142))));
-      double sum = 0.0;
-      for (int i = 0; (i < 6); i = (i + 1)) {
-        double _sum = sum;
-        double _asDouble = actual.get(i).asDouble();
-        Double _get = desire.get(i);
-        double _minus = (_asDouble - (_get).doubleValue());
-        double _pow = Math.pow(_minus, 2.0);
-        sum = (_sum + _pow);
-      }
-      final double norm = Math.sqrt(sum);
-      final double tol = 0.008;
-      if ((norm <= tol)) {
-        final JointTrajectory pose = new JointTrajectory().positions(
-          new double[] { (-0.9546), (-0.20), (-0.7241), 3.1400, 1.6613, (-0.0142) }).jointNames(
-          new String[] { "joint1", "joint2", "joint3", "joint4", "joint5", "joint6" });
-        pub.publish(pose);
-        bridge.unsubscribe("/arm_controller/state");
-      }
-    };
-    bridge.subscribe(
-      SubscriptionRequestMsg.generate("/arm_controller/state").setType("control_msgs/JointTrajectoryControllerState").setThrottleRate(Integer.valueOf(1)).setQueueLength(Integer.valueOf(1)), _function);
+    final List<Double> jointPositions = Collections.<Double>unmodifiableList(CollectionLiterals.<Double>newArrayList(Double.valueOf((-0.9546)), Double.valueOf((-0.20)), Double.valueOf((-0.7241)), Double.valueOf(3.1400), Double.valueOf(1.6613), Double.valueOf((-0.0142))));
+    final JointTrajectory rotateTrajectory = new JointTrajectory().positions(((double[])Conversions.unwrapArray(jointPositions, double.class))).jointNames(
+      new String[] { "joint1", "joint2", "joint3", "joint4", "joint5", "joint6" });
+    in(new Tuple(new Object[] {"getUpCompleted"}), this.self);
+    pub.publish(rotateTrajectory);
   }
 }
