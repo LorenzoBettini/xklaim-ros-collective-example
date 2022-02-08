a1d9f20dcdbbcdbb21948cbab31d3e43707e15bf
tiezzi <francesco.tiezzi@unifi.it>
2022-02-08 16:35:29 +0100

    Refcatoring of Rotate process

--- Robot_arm/src-gen/xklaim/coordination/RobotColl.java
+++ Robot_arm/src-gen/xklaim/coordination/RobotColl.java
@@ -12,7 +12,7 @@ import org.mikado.imc.common.IMCException;
 public class RobotColl extends LogicalNet {
   private static final LogicalLocality Arm = new LogicalLocality("Arm");
   
-  private static final LogicalLocality Robot2 = new LogicalLocality("Robot2");
+  private static final LogicalLocality DeliveryRobot = new LogicalLocality("DeliveryRobot");
   
   public static class Arm extends ClientNode {
     private static class ArmProcess extends KlavaNodeCoordinator {
@@ -26,13 +26,13 @@ public class RobotColl extends LogicalNet {
           this.executeNodeProcess(_grip);
           GetUp _getUp = new GetUp(rosbridgeWebsocketURI);
           this.executeNodeProcess(_getUp);
-          Rotate _rotate = new Rotate(rosbridgeWebsocketURI);
+          Rotate _rotate = new Rotate(rosbridgeWebsocketURI, RobotColl.DeliveryRobot);
           this.executeNodeProcess(_rotate);
-          Lay _lay = new Lay(rosbridgeWebsocketURI, RobotColl.Robot2);
+          Lay _lay = new Lay(rosbridgeWebsocketURI, RobotColl.DeliveryRobot);
           this.executeNodeProcess(_lay);
-          Release _release = new Release(rosbridgeWebsocketURI, RobotColl.Robot2);
+          Release _release = new Release(rosbridgeWebsocketURI, RobotColl.DeliveryRobot);
           this.executeNodeProcess(_release);
-          GoInitialPosition _goInitialPosition = new GoInitialPosition(rosbridgeWebsocketURI, RobotColl.Robot2);
+          GoInitialPosition _goInitialPosition = new GoInitialPosition(rosbridgeWebsocketURI, RobotColl.DeliveryRobot);
           this.executeNodeProcess(_goInitialPosition);
         } catch (Throwable _e) {
           throw Exceptions.sneakyThrow(_e);
@@ -49,8 +49,8 @@ public class RobotColl extends LogicalNet {
     }
   }
   
-  public static class Robot2 extends ClientNode {
-    private static class Robot2Process extends KlavaNodeCoordinator {
+  public static class DeliveryRobot extends ClientNode {
+    private static class DeliveryRobotProcess extends KlavaNodeCoordinator {
       @Override
       public void executeProcess() {
         try {
@@ -64,12 +64,12 @@ public class RobotColl extends LogicalNet {
       }
     }
     
-    public Robot2() {
-      super(new PhysicalLocality("localhost:9999"), new LogicalLocality("Robot2"));
+    public DeliveryRobot() {
+      super(new PhysicalLocality("localhost:9999"), new LogicalLocality("DeliveryRobot"));
     }
     
     public void addMainProcess() throws IMCException {
-      addNodeCoordinator(new RobotColl.Robot2.Robot2Process());
+      addNodeCoordinator(new RobotColl.DeliveryRobot.DeliveryRobotProcess());
     }
   }
   
@@ -79,8 +79,8 @@ public class RobotColl extends LogicalNet {
   
   public void addNodes() throws IMCException {
     RobotColl.Arm arm = new RobotColl.Arm();
-    RobotColl.Robot2 robot2 = new RobotColl.Robot2();
+    RobotColl.DeliveryRobot deliveryRobot = new RobotColl.DeliveryRobot();
     arm.addMainProcess();
-    robot2.addMainProcess();
+    deliveryRobot.addMainProcess();
   }
 }
--- Robot_arm/src-gen/xklaim/coordination/Rotate.java
+++ Robot_arm/src-gen/xklaim/coordination/Rotate.java
@@ -1,26 +1,34 @@
 package xklaim.coordination;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import coordination.JointTrajectory;
 import java.util.Collections;
 import java.util.List;
+import klava.Locality;
 import klava.Tuple;
 import klava.topology.KlavaProcess;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Conversions;
 import ros.Publisher;
 import ros.RosBridge;
+import ros.RosListenDelegate;
+import ros.SubscriptionRequestMsg;
 
 @SuppressWarnings("all")
 public class Rotate extends KlavaProcess {
   private String rosbridgeWebsocketURI;
   
-  public Rotate(final String rosbridgeWebsocketURI) {
+  private Locality deliveryRobot;
+  
+  public Rotate(final String rosbridgeWebsocketURI, final Locality deliveryRobot) {
     super("xklaim.coordination.Rotate");
     this.rosbridgeWebsocketURI = rosbridgeWebsocketURI;
+    this.deliveryRobot = deliveryRobot;
   }
   
   @Override
   public void executeProcess() {
+    final Locality local = this.self;
     final RosBridge bridge = new RosBridge();
     bridge.connect(this.rosbridgeWebsocketURI, true);
     final Publisher pub = new Publisher("/arm_controller/command", "trajectory_msgs/JointTrajectory", bridge);
@@ -29,5 +37,26 @@ public class Rotate extends KlavaProcess {
       new String[] { "joint1", "joint2", "joint3", "joint4", "joint5", "joint6" });
     in(new Tuple(new Object[] {"getUpCompleted"}), this.self);
     pub.publish(rotateTrajectory);
+    final RosListenDelegate _function = (JsonNode data, String stringRep) -> {
+      final JsonNode actual = data.get("msg").get("actual").get("positions");
+      double delta = 0.0;
+      final double tolerance = 0.008;
+      for (int i = 0; (i < jointPositions.size()); i++) {
+        double _delta = delta;
+        double _asDouble = actual.get(i).asDouble();
+        Double _get = jointPositions.get(i);
+        double _minus = (_asDouble - (_get).doubleValue());
+        double _pow = Math.pow(_minus, 2.0);
+        delta = (_delta + _pow);
+      }
+      final double norm = Math.sqrt(delta);
+      if ((norm <= tolerance)) {
+        out(new Tuple(new Object[] {"rotationCompleted"}), local);
+        out(new Tuple(new Object[] {"goto1", "world", (-0.25), (-2.67), 1.0}), this.deliveryRobot);
+        bridge.unsubscribe("/arm_controller/state");
+      }
+    };
+    bridge.subscribe(
+      SubscriptionRequestMsg.generate("/arm_controller/state").setType("control_msgs/JointTrajectoryControllerState").setThrottleRate(Integer.valueOf(1)).setQueueLength(Integer.valueOf(1)), _function);
   }
 }
